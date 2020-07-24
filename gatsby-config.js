723c40769c70e1cc22e59d8cc3594e3b020a633c
vubadien <nhan.phan@qupworld.com>
2020-07-24 16:11:11 +0700

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
+const dotenv = require("dotenv").config();
 
 module.exports = {
   /* Your site config here */
@@ -15,9 +16,9 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
         // Learn about environment variables: https://gatsby.dev/env-vars
-        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
     `gatsby-plugin-sass`,
